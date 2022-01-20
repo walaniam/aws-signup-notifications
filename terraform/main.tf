da326bf9626140f5ae7302ac37e58e3798594fdd
Mariusz Walania <mariusz.walania@gmail.com>
2022-01-20 21:54:44 +0100

    terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,14 +13,14 @@ provider "aws" {
 }
 
 resource "aws_sqs_queue" "signups_queue" {
-  name                      = "signups_queue"
-  message_retention_seconds = 345600
-  delay_seconds             = 300
+  name                       = "signups_queue"
+  message_retention_seconds  = 345600
+  delay_seconds              = 300
   visibility_timeout_seconds = 45
 }
 
 resource "aws_sns_topic_subscription" "signups_sqs_target" {
-    topic_arn = var.sns_signups_arn
-    protocol = "sqs"
-    endpoint = aws_sqs_queue.signups_queue.arn
+  topic_arn = var.sns_signups_arn
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.signups_queue.arn
 }
