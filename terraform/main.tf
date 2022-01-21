49854df61f6ab099ff25b32c46bead40260f4654
Mariusz Walania <mariusz.walania@gmail.com>
2022-01-21 11:13:35 +0100

    sqs topic policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,6 +19,30 @@ resource "aws_sqs_queue" "signups_queue" {
   visibility_timeout_seconds = 45
 }
 
+resource "aws_sqs_queue_policy" "signups_queue_policy" {
+  queue_url = aws_sqs_queue.signups_queue.id
+  policy    = <<POLICY
+    {
+      "Version": "2012-10-17",
+      "Id": "sqspolicy",
+      "Statement": [
+        {
+          "Sid": "First",
+          "Effect": "Allow",
+          "Principal": "*",
+          "Action": "sqs:SendMessage",
+          "Resource": "${aws_sqs_queue.signups_queue.arn}",
+          "Condition": {
+            "ArnEquals": {
+              "aws:SourceArn": "${var.sns_signups_arn}"
+            }
+          }
+        }
+      ]
+    }
+  POLICY
+}
+
 resource "aws_sns_topic_subscription" "signups_sqs_target" {
   topic_arn = var.sns_signups_arn
   protocol  = "sqs"
