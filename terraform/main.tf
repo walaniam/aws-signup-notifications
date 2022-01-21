dffb0cd9ce56bff894c8293e1093b3c0f743831e
Mariusz Walania <mariusz.walania@gmail.com>
2022-01-21 22:07:00 +0100

    lambda trigger and fixed logging

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,6 +12,7 @@ provider "aws" {
   region  = "eu-west-1"
 }
 
+# SQS queue for signup events
 resource "aws_sqs_queue" "signups_queue" {
   name                       = "signups_queue"
   message_retention_seconds  = 43200
@@ -21,30 +22,112 @@ resource "aws_sqs_queue" "signups_queue" {
 
 resource "aws_sqs_queue_policy" "signups_queue_policy" {
   queue_url = aws_sqs_queue.signups_queue.id
-  policy    = <<POLICY
+  policy    = <<EOF
+{
+  "Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement": [
     {
-      "Version": "2012-10-17",
-      "Id": "sqspolicy",
-      "Statement": [
-        {
-          "Sid": "First",
-          "Effect": "Allow",
-          "Principal": "*",
-          "Action": "sqs:SendMessage",
-          "Resource": "${aws_sqs_queue.signups_queue.arn}",
-          "Condition": {
-            "ArnEquals": {
-              "aws:SourceArn": "${var.sns_signups_arn}"
-            }
-          }
+      "Sid": "First",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.signups_queue.arn}",
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${var.sns_signups_arn}"
         }
-      ]
+      }
     }
-  POLICY
+  ]
+}
+EOF
 }
 
+# Subscribe SQS queue to SNS topic
 resource "aws_sns_topic_subscription" "signups_sqs_target" {
   topic_arn = var.sns_signups_arn
   protocol  = "sqs"
   endpoint  = aws_sqs_queue.signups_queue.arn
+}
+
+# Lambda function
+resource "aws_lambda_function" "signups_processing_function" {
+  function_name    = "signups_processing_function"
+  filename         = var.lambda_jar_file
+  source_code_hash = base64sha256(filebase64(var.lambda_jar_file))
+  handler          = var.lambda_function_handler
+  runtime          = var.lambda_runtime
+  role             = aws_iam_role.lambda_role.arn
+}
+
+# Lambda trigger
+resource "aws_lambda_event_source_mapping" "sqs_to_lambda" {
+  event_source_arn = aws_sqs_queue.signups_queue.arn
+  function_name    = aws_lambda_function.signups_processing_function.arn
+  enabled          = false
+}
+
+# Lambda role
+resource "aws_iam_role" "lambda_role" {
+  name               = "LambdaRole"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+        "Action": "sts:AssumeRole",
+        "Effect": "Allow",
+        "Principal": {
+            "Service": "lambda.amazonaws.com"
+        }
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "lambda_role_sqs_policy" {
+  name   = "AllowSQSPermissions"
+  role   = aws_iam_role.lambda_role.id
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+              "sqs:GetQueueAttributes",
+              "sqs:DeleteMessage",
+              "sqs:ChangeMessageVisibility",
+              "sqs:ReceiveMessage",
+              "sqs:TagQueue",
+              "sqs:UntagQueue"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "lambda_role_logs_policy" {
+  name   = "AllowLogging"
+  role   = aws_iam_role.lambda_role.id
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,3 +5,15 @@ variable "sns_signups_arn" {
   type        = string
   sensitive   = true
 }
+
+variable "lambda_jar_file" {
+  default = "../target/signup-notifications-1.0-SNAPSHOT.jar"
+}
+
+variable "lambda_function_handler" {
+  default = "walaniam.aws.signup.SignupNotificationsHandler"
+}
+
+variable "lambda_runtime" {
+  default = "java8"
+}
